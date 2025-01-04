e0c101d87f85497ad5c9f9343d4a054c74b3be05
tommy <93843172+tommybraaschh@users.noreply.github.com>
2025-01-03 23:02:52 -0600

    Add files via upload
    updated code

--- lkiller.sh
+++ lkiller.sh
@@ -2,13 +2,18 @@ echo Starting Life360 Killer
 echo Checking for root privileges 
 id
 echo Running as root!
+sleep 1
 echo Starting killer...
 killall Life360
 echo Terminated Life360 loaded in memory
+sleep 2
 mkdir /var/mobile/Life360BACK
 cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
+echo Removing Life360 binary from disk
 cp Life360 /var/mobile/Life360BACK
 cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
+sleep 3
 rm Life360
+sleep 1  
 echo Removed Life360 binary from disk
 echo Successfully killed Life360 
