1fea9980ea6f699ae5671b95d1d4f9b6d097a29d
tommy <93843172+t0pcart@users.noreply.github.com>
2025-01-15 09:44:51 -0600

    Update lstart.sh

--- lstart.sh
+++ lstart.sh
@@ -1,9 +1,12 @@
 echo Starting Life360/FindMy Reloader 
-echo Moving binary into application path
+echo Checking for root
+id
+echo Running as root!
+echo Moving Life360 binary into application path
 cd /var/mobile/Life360BACK
 cp Life360 /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
 echo Moved binary to application path 
-echo Spawning Life360 as a mobile process under PID 8720
+echo Spawning Life360 as a mobile process
 echo Restarting FindMy Daemaon
 launchctl start findmydeviced 
 open com.life360.safetymap
