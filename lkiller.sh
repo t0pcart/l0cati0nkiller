07e077ea1486e54faf4ea8c52ca226b888bf296f
tommy <93843172+t0pcart@users.noreply.github.com>
2025-01-14 21:07:06 -0600

    removed auto root in favor of sudo

--- lkiller.sh
+++ lkiller.sh
@@ -1,16 +1,15 @@
 echo Starting Life360 Killer
-echo Spawning as root
-sleep 1
-sudo
-echo Spwaned as root!
+echo Getting root
+id
+echo Got root
 echo Starting killer...
 killall Life360
 echo Terminated Life360 loaded in memory
 mkdir /var/mobile/Life360BACK
-sudo cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
-echo Removing Life360 binary from disk
-sudo cp Life360 /var/mobile/Life360BACK
 cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
-sudo rm Life360  
+echo Removing Life360 binary from disk
+cp Life360 /var/mobile/Life360BACK
+cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
+rm Life360  
 echo Removed Life360 binary from disk
 echo Successfully killed Life360 
