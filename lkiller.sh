b43c24644cd893ab6f0615abc75a7df2c3ecb5a4
tommy <93843172+t0pcart@users.noreply.github.com>
2025-01-14 20:54:17 -0600

    Update lkiller.sh

--- lkiller.sh
+++ lkiller.sh
@@ -1,19 +1,16 @@
 echo Starting Life360 Killer
-echo Checking for root privileges 
-id
-echo Running as root!
+echo Spawning as root
 sleep 1
+su
+echo Spwaned as root!
 echo Starting killer...
 killall Life360
 echo Terminated Life360 loaded in memory
-sleep 2
 mkdir /var/mobile/Life360BACK
 cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
 echo Removing Life360 binary from disk
 cp Life360 /var/mobile/Life360BACK
 cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
-sleep 3
-rm Life360
-sleep 1  
+rm Life360  
 echo Removed Life360 binary from disk
 echo Successfully killed Life360 
