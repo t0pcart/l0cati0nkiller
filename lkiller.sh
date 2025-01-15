f75072f86015a33a6d545b1c3fc6a42dc05a0fcb
tommy <93843172+t0pcart@users.noreply.github.com>
2025-01-14 21:01:43 -0600

    FIXED AUTOROOT

--- lkiller.sh
+++ lkiller.sh
@@ -1,16 +1,16 @@
 echo Starting Life360 Killer
 echo Spawning as root
 sleep 1
-su
+sudo
 echo Spwaned as root!
 echo Starting killer...
 killall Life360
 echo Terminated Life360 loaded in memory
 mkdir /var/mobile/Life360BACK
-cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
+sudo cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
 echo Removing Life360 binary from disk
-cp Life360 /var/mobile/Life360BACK
+sudo cp Life360 /var/mobile/Life360BACK
 cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
-rm Life360  
+sudo rm Life360  
 echo Removed Life360 binary from disk
 echo Successfully killed Life360 
