0a263fe2be9599b9a02ab1674416ffed31aca816
tommy <93843172+t0pcart@users.noreply.github.com>
2025-01-14 21:05:54 -0600

    remove autoroot in favor of using sudo

--- lstart.sh
+++ lstart.sh
@@ -1,11 +1,9 @@
-
 echo Starting Life360/FindMy Starter
-echo Spawning script as root
-sudo
-sleep 1
-sudo echo Spawned script as root
-sudo echo Reinstalling binary onto disk
-sudo cd /var/mobile/Life360BACK
-sudo cp Life360 /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
-sudo echo Spawning Life360 as a mobile process
+echo Getting root
+id
+echo Got root
+echo Reinstalling binary onto disk
+cd /var/mobile/Life360BACK
+cp Life360 /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
+echo Spawning Life360 as a mobile process
 open com.life360.safetymap
