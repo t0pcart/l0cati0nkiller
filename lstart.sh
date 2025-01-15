07c53486c2f74707cf583293a6a4eeda9b73261b
tommy <93843172+t0pcart@users.noreply.github.com>
2025-01-14 20:56:16 -0600

    make the script request root automatically

--- lstart.sh
+++ lstart.sh
@@ -1,4 +1,9 @@
-echo Starting Life360 Reloader 
+
+echo Starting Life360/FindMy Starter
+echo Spawning script as root
+sleep 1
+su
+echo Spawned script as root
 echo Reinstalling binary onto disk
 cd /var/mobile/Life360BACK
 cp Life360 /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
