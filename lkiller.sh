61c8c9041f04a379954ae78a97377d03bcbe9978
tommy <93843172+t0pcart@users.noreply.github.com>
2025-01-15 20:36:08 -0600

    Update lkiller.sh

--- lkiller.sh
+++ lkiller.sh
@@ -6,12 +6,14 @@ echo Starting killer...
 killall Life360
 killall findmydeviced
 Launchctl stop findmydeviced
+echo Unloaded findmydeviced 
 echo Terminated Life360/FindMy loaded in memory
-mkdir /var/mobile/Life360BACK
+mkdir /var/mobile/BACK
 cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
 echo Removing Life360 binary from disk
-cp Life360 /var/mobile/Life360BACK
+cp Life360 /var/mobile/BACK
 cd /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A264E/Life360.app
 rm Life360  
 echo Removed Life360 binary from disk
+echo Successfull
 echo Successfully killed Life360/FindMy 
