6aa4bf1940f92a7a36e3f0572e83ec1e9adfef07
tommy <93843172+t0pcart@users.noreply.github.com>
2025-01-20 13:08:05 -0600

    Update lstart.sh

--- lstart.sh
+++ lstart.sh
@@ -8,5 +8,7 @@ cp Life360 /var/containers/Bundle/Application/1F98E7D8-0F9B-46FE-90E0-9E582B1A26
 echo Moved binary to application path 
 echo Spawning Life360 as a mobile process
 echo Restarting FindMy Daemaon
-launchctl start findmydeviced 
+launchctl load findmydeviced 
 open com.life360.safetymap
+sleep 1
+launchctl userspace reboot
